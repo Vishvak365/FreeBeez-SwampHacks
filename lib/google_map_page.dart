43bcce535b986df36a7ec1c9d1247da93f21097e
AddisonBiaggi <addisonbiaggi@gmail.com>
2020-02-01 02:45:17 -0500

    button post to db

---
+++ lib/google_map_page.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+GoogleMap gmap() {
+  Completer<GoogleMapController> _controller = Completer();
+  List<Marker> allMarkers = [];
+  const LatLng _center = const LatLng(29.6479375, -82.3440625);
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  allMarkers.add(Marker(
+      markerId: MarkerId('myMarker'),
+      draggable: true,
+      onTap: () {
+        print('Marker Tapped');
+      },
+      position: _center));
+
+  return GoogleMap(
+    onMapCreated: _onMapCreated,
+    initialCameraPosition: CameraPosition(
+      target: LatLng(29.6479375, -82.3440625),
+      zoom: 18.0,
+    ),
+    markers: Set.from(allMarkers),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'google_map_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,42 +12,47 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Completer<GoogleMapController> _controller = Completer();
-  List<Marker> allMarkers = [];
-  static const LatLng _center = const LatLng(29.6479375, -82.3440625);
-
-  void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-  }
-
-  void initState() {
-    super.initState();
-    allMarkers.add(Marker(
-        markerId: MarkerId('myMarker'),
-        draggable: true,
-        onTap: () {
-          print('Marker Tapped');
-        },
-        position: _center));
-  }
-
   @override
+  final databaseReference = Firestore.instance;
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: LatLng(29.6479375, -82.3440625),
-            zoom: 18.0,
-          ),
-          markers: Set.from(allMarkers),
+            title: Text('FreeBeez'),
+            backgroundColor: Colors.blue[700],
+            actions: <Widget>[
+              // action button
+              IconButton(
+                icon: Icon(Icons.local_post_office),
+                onPressed: () {
+                  createRecord();
+                },
+              )
+            ]),
+        body: gmap(),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0, // this will be set when a new tab is tapped
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.filter),
+              title: new Text('Filter'),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.local_post_office),
+              title: new Text('Post'),
+            )
+          ],
         ),
       ),
     );
+  }
+
+  void createRecord() async {
+    DocumentReference ref = await databaseReference
+        .collection("postings")
+        .add({'title': 'Button Post', 'description': 'Eat it flutter!'});
+    print(ref.documentID);
+    
   }
 }
