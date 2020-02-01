5cde34b8c5342d8c9984b713f3c28ddba790ed03
Vishvak Seenichamy <vishvak365@gmail.com>
2020-02-01 14:52:11 -0500

    Implemented Pop Up Menu

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'google_map_page.dart';
 import 'package:geolocator/geolocator.dart';
+import 'queryForm.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(title: "RaisedButton", home: MyApp()));
 //void main() => runApp(AddDataToFireStore());
 final databaseReference = Firestore.instance;
 
@@ -64,61 +66,93 @@ class _MyAppState extends State<MyApp> {
 
   @override
   final databaseReference = Firestore.instance;
-
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: FutureBuilder(
-          builder: (context, snapshot) {
-            if (snapshot != null) {
-              return GoogleMap(
+    return Scaffold(
+      body: FutureBuilder(
+        builder: (context, snapshot) {
+          if (snapshot != null) {
+            return Container(
+              child: GoogleMap(
                 onMapCreated: _onMapCreated,
                 initialCameraPosition: CameraPosition(
                   target: LatLng(29.6479375, -82.3440625),
                   zoom: 18.0,
                 ),
                 markers: Set.from(allMarkers),
-              );
-            }
-            return CircularProgressIndicator();
-          },
-          future: getData(),
-        ),
-        appBar: AppBar(
-            title: Text('FreeBeez'),
-            backgroundColor: Colors.blue[700],
-            actions: <Widget>[
-              // action button
-              IconButton(
-                icon: Icon(Icons.local_post_office),
-                onPressed: () {
-                  createRecord();
-                },
-              )
-            ]),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0, // this will be set when a new tab is tapped
-          items: [
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.filter),
-              title: new Text('Filter'),
-            ),
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.local_post_office),
-              title: new Text('Post'),
+              ),
+            );
+          }
+          return CircularProgressIndicator();
+        },
+        future: getData(),
+      ),
+      appBar: AppBar(
+          title: Text('FreeBeez'),
+          backgroundColor: Colors.blue[700],
+          actions: <Widget>[
+            // action button
+            IconButton(
+              icon: Icon(Icons.filter_list),
+              onPressed: () {
+                queryData();
+              },
             )
-          ],
-        ),
+          ]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0, // this will be set when a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.filter),
+            title: new Text('Filter'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.local_post_office),
+            title: new Text('Post'),
+          )
+        ],
       ),
     );
   }
 
+  void queryData() {
+    Alert(
+      context: context,
+      type: AlertType.warning,
+      title: "RFLUTTER ALERT",
+      desc: "Flutter is more awesome with RFlutter Alert.",
+      buttons: [
+        DialogButton(
+          child: Text(
+            "FLAT",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () => Navigator.pop(context),
+          color: Color.fromRGBO(0, 179, 134, 1.0),
+        ),
+        DialogButton(
+          child: Text(
+            "GRADIENT",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () => Navigator.pop(context),
+          gradient: LinearGradient(colors: [
+            Color.fromRGBO(116, 116, 191, 1.0),
+            Color.fromRGBO(52, 138, 199, 1.0)
+          ]),
+        )
+      ],
+    ).show();
+  }
+
   void createRecord() async {
+    print("--------------------------------------------");
     Position position = await Geolocator()
         .getLastKnownPosition(desiredAccuracy: LocationAccuracy.high);
-
+    print(position);
     GeoPoint coordinates = new GeoPoint(position.latitude, position.longitude);
 
+    print(coordinates);
     DocumentReference ref = await databaseReference
         .collection("postings")
         .add({'loc': coordinates});
